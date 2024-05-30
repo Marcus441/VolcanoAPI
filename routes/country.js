e68d282b1bfa7cd4cd315cb198c3be71f6392951
Marcus441 <psminchia4153@hotmail.com>
2024-05-30 14:04:45 +1000

    made population data at country endpoint int

--- routes/country.js
+++ routes/country.js
@@ -4,12 +4,13 @@ var router = express.Router();
 
 const noQueryParamsAllowed = require('../middleware/noQueryParamsAllowed');
 
-router.get("/:country", noQueryParamsAllowed,function (req, res, next) {
+router.get("/:country", noQueryParamsAllowed, function (req, res, next) {
     const country = req.params.country;
 
     req.db
         .from('data')
         .where('country', country)
+        .select('country')
         .count('id as volcanoCount')
         .min('elevation as minElevation')
         .max('elevation as maxElevation')
@@ -19,10 +20,14 @@ router.get("/:country", noQueryParamsAllowed,function (req, res, next) {
         .sum('population_100km as population100km')
         .first()
         .then((data) => {
-            if (data.volcanoCount === 0) {
+            if (!data || data.volcanoCount === 0) {
                 return next(new Error(`No data found for country: ${country}`, 404));
             }
-            res.json(data);
+            data.population5km = parseInt(data.population5km);
+            data.population10km = parseInt(data.population10km);
+            data.population30km = parseInt(data.population30km);
+            data.population100km = parseInt(data.population100km);
+            res.json({ ...data });
         }).catch((err) => {
             next(err);
         });
